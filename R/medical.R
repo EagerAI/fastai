3fa3f18be4d6f6dd0638767981060dd1d5c9c6c2
henry090 <turqut.a.314@gmail.com>
2020-08-30 16:32:50 +0400

    dicom ops

--- R/medical.R
+++ R/medical.R
@@ -37,6 +37,72 @@ dcmread <- function(fn, force = FALSE) {
 }
 
 
+#' @title Dicom
+#'
+#' @param img dicom file
+#'
+#' @export
+print.pydicom.dataset.FileDataset <- function(img) {
+  cat(img$`__str__`())
+}
+
+
+#' @title Get image matrix
+#'
+#' @param img dicom file
+#' @param type img transformation
+#'
+#' @export
+get_dcm_matrix <- function(img, type = 'raw') {
+
+  obj = medical$dicom_windows
+
+  if (type=='raw') {
+    img$pixel_array
+  } else if (type=='abdomen_soft') {
+    res = obj$abdomen_soft
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='brain') {
+    res = obj$brain
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='brain_bone') {
+    res = obj$brain_bone
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='brain_soft') {
+    res = obj$brain_soft
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='liver') {
+    res = obj$liver
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='lungs') {
+    res = obj$lungs
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='mediastinum') {
+    res = obj$mediastinum
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='spine_bone') {
+    res = obj$spine_bone
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='spine_soft') {
+    res = obj$spine_soft
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='stroke') {
+    res = obj$stroke
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='subdural') {
+    res = obj$subdural
+    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+  } else if (type=='normalized') {
+    img$scaled_px$hist_scaled()$cpu()$numpy()
+  }
+
+}
+
+
+
+
+
+
 
 
 
