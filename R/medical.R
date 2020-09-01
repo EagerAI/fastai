0c80935c6fd5f2647f102b31f39a80831fb82774
henry090 <turqut.a.314@gmail.com>
2020-09-01 15:55:23 +0400

    fix get_dcm_matrix

--- R/medical.R
+++ R/medical.R
@@ -53,56 +53,216 @@ print.pydicom.dataset.FileDataset <- function(img) {
 #' @param type img transformation
 #'
 #' @export
-get_dcm_matrix <- function(img, type = 'raw') {
+get_dcm_matrix <- function(img, type = 'raw', scan = '', size = 50, convert = TRUE) {
 
   obj = medical$dicom_windows
+
+  fun = function() {
+    if(scan=='uniform_blur2d') {
+      img = medical$uniform_blur2d(
+        img$scaled_px$windowed(res[[1]], res[[2]]), as.integer(size))
+    } else if(scan=='gauss_blur2d') {
+      img = medical$gauss_blur2d(
+        img$scaled_px$windowed(res[[1]], res[[2]]), as.integer(size))
+    } else {
+      img = img$scaled_px$windowed(res[[1]], res[[2]])
+    }
+
+    if(convert) {
+      img$cpu()$numpy()
+    } else {
+      img
+    }
+  }
 
   if (type=='raw') {
     img$pixel_array
   } else if (type=='abdomen_soft') {
     res = obj$abdomen_soft
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='brain') {
     res = obj$brain
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='brain_bone') {
     res = obj$brain_bone
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='brain_soft') {
     res = obj$brain_soft
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='liver') {
     res = obj$liver
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='lungs') {
     res = obj$lungs
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='mediastinum') {
     res = obj$mediastinum
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='spine_bone') {
     res = obj$spine_bone
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='spine_soft') {
     res = obj$spine_soft
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='stroke') {
     res = obj$stroke
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='subdural') {
     res = obj$subdural
-    img$scaled_px$windowed(res[[1]], res[[2]])$cpu()$numpy()
+    fun()
   } else if (type=='normalized') {
-    img$scaled_px$hist_scaled()$cpu()$numpy()
+    if(scan=='uniform_blur2d') {
+      img = medical$uniform_blur2d(
+        img$scaled_px$hist_scaled(), as.integer(size))
+    } else if(scan=='gauss_blur2d') {
+      img = medical$gauss_blur2d(
+        img$scaled_px$hist_scaled(), as.integer(size))
+    } else {
+      img = img$hist_scaled()
+    }
+
+    if (convert) {
+      img$cpu()$numpy()
+    } else {
+      img
+    }
   }
 
 }
 
 
+#' @title mask_from_blur
+#'
+#'
+#' @param x x
+#' @param window window
+#' @param sigma sigma
+#' @param thresh thresh
+#' @param remove_max remove_max
+#'
+#' @export
+mask_from_blur <- function(img, window, sigma = 0.3, thresh = 0.05, remove_max = TRUE) {
 
+  img$mask_from_blur(
+    window = list(as.integer(window[[1]]),as.integer(window[[2]])),
+    sigma = sigma,
+    thresh = thresh,
+    remove_max = remove_max
+  )
 
+}
 
+#' @title Zoom
+#'
+#'
+#'
+#' @param ratio ratio
+#'
+#' @export
+zoom <- function(img, ratio) {
 
+  img$zoom(
+    ratio = ratio
+  )
+
+}
+
+#' @title Mask2bbox
+#'
+#'
+#' @param mask mask
+#'
+#' @export
+mask2bbox <- function(mask, convert = TRUE) {
+
+  res = medical$mask2bbox(
+    mask = mask
+  )
+
+  if(convert) {
+    lo = res[[0]]$cpu()$numpy()
+    hi = res[[1]]$cpu()$numpy()
+    list(lo,hi)
+  } else {
+    res
+  }
+
+}
+
+#' @title Abdomen soft
+#'
+#' @export
+win_abdoment_soft <- function() {
+  medical$dicom_windows$abdomen_soft
+}
+
+#' @title Brain
+#'
+#' @export
+win_brain <- function() {
+  medical$dicom_windows$brain
+}
+
+#' @title Brain bone
+#'
+#' @export
+win_brain_bone <- function() {
+  medical$dicom_windows$brain_bone
+}
+
+#' @title Brain soft
+#'
+#' @export
+win_brain_soft <- function() {
+  medical$dicom_windows$brain_soft
+}
+
+#' @title Liver
+#'
+#' @export
+win_liver <- function() {
+  medical$dicom_windows$liver
+}
+
+#' @title Lungs
+#'
+#' @export
+win_lungs <- function() {
+  medical$dicom_windows$lungs
+}
+#' @title Mediastinum
+#'
+#' @export
+win_mediastinum <- function() {
+  medical$dicom_windows$mediastinum
+}
+
+#' @title Spine bone
+#'
+#' @export
+win_spine_bone <- function() {
+  medical$dicom_windows$spine_bone
+}
+#' @title Spine soft
+#'
+#' @export
+win_spine_soft <- function() {
+  medical$dicom_windows$spine_soft
+}
+
+#' @title Stroke
+#'
+#' @export
+win_stroke <- function() {
+  medical$dicom_windows$stroke
+}
+
+#' @title Subdural
+#'
+#' @export
+win_subdural <- function() {
+  medical$dicom_windows$subdural
+}
 
 
 
