8d3a00d3d217dfda40ca2cc4ce6dd516816112ca
henry090 <turqut.a.314@gmail.com>
2020-09-11 23:13:41 +0400

    add show_batch

--- R/test.R
+++ R/test.R
@@ -50,5 +50,41 @@ plot.to_fastai_training_history <- function(history) {
 }
 
 
+#' @title show_batch
+#'
+#' @description
+#'
+#' @param dls dataloader object
+#' @param b b
+#' @param max_n max_n
+#' @param ctxs ctxs
+#' @param show show
+#' @param unique unique
+#'
+#' @export
+show_batch <- function(dls, b = NULL, max_n = 9, ctxs = NULL,
+                       show = TRUE, unique = FALSE, dpi = 90) {
+
+  dls$show_batch(
+    b = b,
+    max_n = as.integer(max_n),
+    ctxs = ctxs,
+    show = show,
+    unique = unique
+  )
+
+  tmp_d = proj_name = gsub(tempdir(), replacement = '/', pattern = '\\', fixed=TRUE)
+  fastai2$tabular$all$plt$savefig(paste(tmp_d, 'test.png', sep = '/'), dpi = as.integer(dpi))
+
+  img <- readPNG(paste(tmp_d, 'test.png', sep = '/'))
+  lim <- par()
+  plot.new()
+  rasterImage(img, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4])
+  invisible(file.remove(paste(tmp_d, 'test.png', sep = '/')))
+}
+
+
+
+
 
 
