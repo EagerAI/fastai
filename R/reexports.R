12fa46d7c60e379cee33f3f8083c230204535971
henry090 <turqut.a.314@gmail.com>
2020-09-26 12:56:21 +0400

    fix

--- R/reexports.R
+++ R/reexports.R
@@ -9,3 +9,24 @@ stats::predict
 #' @importFrom stats median
 #' @export
 stats::median
+
+#' @importFrom grDevices dev.off
+#' @export
+grDevices::dev.off
+
+#' @importFrom stats sd
+#' @export
+stats::sd
+
+#' @importFrom utils capture.output
+#' @export
+utils::capture.output
+
+
+#' @importFrom utils download.file
+#' @export
+utils::download.file
+
+
+
+
