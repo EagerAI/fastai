ac8277f1f8751f5890d920c78e00f7714af60adf
henry090 <turqut.a.314@gmail.com>
2020-10-29 19:58:08 +0400

    skip 32 bit. Cran is extremely slow

--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -4,10 +4,15 @@ skip_if_no_fastai_core <- function() {
     skip("fastai not available for testing")
 }
 
+skip_if_32bit <- function() {
+  if (Sys.info()[["machine"]]=="x86_32")
+    skip("skip 32 bit system!")
+}
 
 test_succeeds <- function(desc, expr) {
   test_that(desc, {
     skip_if_no_fastai_core()
+    skip_if_32bit()
     expect_error(force(expr), NA)
   })
 }
