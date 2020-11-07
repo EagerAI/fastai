af8cce1fc6c615f924b7bc5f7e38ff382d28ff4b
henry090 <turqut.a.314@gmail.com>
2020-11-07 18:01:09 +0400

    try to fix windows

--- tests/testthat/test-modify.R
+++ tests/testthat/test-modify.R
@@ -6,7 +6,8 @@ source("utils.R")
 test_succeeds('modify scalar tensor', {
   x3 = tensor(list(list(c(1, 2, 3), c(4, 5, 6), c(7, 8, 9)),list(c(10, 11, 12), c(13, 14, 15), c(16, 17, 18))))
   init = tensor(list(list(c(1, 2, 3), c(4, 5, 6), c(7, 8, 9)),list(c(10, 11, 12), c(13, 14, 15), c(16, 17, 18))))
-  x3[0][0][1] %f% 12
+  value = as.numeric(12)
+  x3[0][0][1] %f% value
   expect_equal(init[0][0][1]$cpu()$numpy() + 10, x3[0][0][1]$cpu()$numpy())
   expect_equal(init[0][0][1] + 10, x3[0][0][1])
 })
