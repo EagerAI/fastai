40217cd5a9718fc05681dbf3fc5734465ab576ec
henry090 <turqut.a.314@gmail.com>
2020-11-08 14:52:27 +0400

    evaluate slicing similar to python. So users will not be confused

--- R/experimental.R
+++ R/experimental.R
@@ -19,7 +19,7 @@
       left_expr = paste(deparse(substitute(left)), right, sep = '<-')
     }
     return(try(eval(parse(text = left_expr)), TRUE))
-  } else {
+  }  else {
     #deparse(substitute(x))
 
     cls = right
@@ -40,6 +40,41 @@
   }
 
 }
+
+#' @title Modify tensor
+#'
+#' @param tensor torch tensor
+#' @return tensor
+#' @export
+E = function(tensor) {
+  # get slice
+  string = deparse(substitute(a[,,,1]))
+
+  string = gsub(" ", "", string, fixed = TRUE)
+
+  string = gsub(",", ":,:", string)
+
+  string = gsub('(.)\\1+', '\\1', string)
+
+  # get tensor object
+  a2 = sub("\\[.*", "", string)
+
+  a = eval(parse(text = a2))
+
+  # tempdir
+  temp = tempdir()
+  torch()$save(a, paste(temp,'torch_a',sep = '/'))
+
+  reticulate::py_run_string(glue::glue("
+import torch
+a = torch.load('{temp}/torch_a')
+a = {string}
+torch.save(a, '{temp}/torch_a')
+                          "))
+  left = torch()$load(paste(temp,'torch_a',sep = '/'))
+  return(left)
+}
+
 
 
 
