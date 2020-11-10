215c9c82c97ca4d7501e9cf3935d22b16188206e
henry090 <turqut.a.314@gmail.com>
2020-11-10 13:33:11 +0400

    add nn_module example

--- vignettes/low.R
+++ vignettes/low.R
@@ -22,12 +22,15 @@ knitr::opts_chunk$set(echo = TRUE,eval = FALSE,echo = T)
 
 ## -----------------------------------------------------------------------------
 #  print(learn$model[0][0][0])
-#  # Conv2d(3, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False))
+#  # Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False))
 #  learn$model[0][0][0][['kernel_size']] %f%  reticulate::tuple(list(9L,9L))
+#  print(learn$model[0][0][0])
 #  # Conv2d(1, 32, kernel_size=(9, 9), stride=(2, 2), padding=(1, 1), bias=False)
 
 ## -----------------------------------------------------------------------------
 #  x = tensor(c(1,2), c(3,4))
+#  # tensor([[1., 2.],
+#  #         [3., 4.]])
 #  print(x[0][0])
 #  # tensor(1.)
 #  
@@ -35,6 +38,10 @@ knitr::opts_chunk$set(echo = TRUE,eval = FALSE,echo = T)
 #  x[0][0] %f% 99
 #  print(x[0][0])
 #  # tensor(99.)
+#  
+#  print(x)
+#  # tensor([[99.,  2.],
+#  #         [ 3.,  4.]])
 
 ## -----------------------------------------------------------------------------
 #  print(x[0])
@@ -42,4 +49,84 @@ knitr::opts_chunk$set(echo = TRUE,eval = FALSE,echo = T)
 #  # change to 55, 55
 #  x[0] %f% c(55,55)
 #  # tensor([55., 55.])
+
+## -----------------------------------------------------------------------------
+#  a = tensor(array(1:100, c(3,3,3,3)))
+#  a$shape
+#  # torch.Size([3, 3, 3, 3])
+
+## -----------------------------------------------------------------------------
+#  a %>% narrow('[0,:,:,:]')
+
+## -----------------------------------------------------------------------------
+#  a %>% narrow("[:,0,:,:]")
+
+## -----------------------------------------------------------------------------
+#  a %>% narrow('[:,0,0,:]')
+
+## -----------------------------------------------------------------------------
+#  a %>% narrow("[1,1,1,:]")
+
+## -----------------------------------------------------------------------------
+#  library(magrittr)
+#  library(fastai)
+#  library(zeallot)
+#  
+#  if(!file.exists('mnist.pkl.gz')) {
+#    download.file('http://deeplearning.net/data/mnist/mnist.pkl.gz','mnist.pkl.gz')
+#    R.utils::gunzip("mnist.pkl.gz", remove=FALSE)
+#  }
+#  
+#  c(c(x_train, y_train), c(x_valid, y_valid), res) %<-%
+#    reticulate::py_load_object('mnist.pkl', encoding = 'latin-1')
+#  
+#  x_train = x_train[1:500,1:784]
+#  x_valid = x_valid[1:500,1:784]
+#  
+#  y_train = as.integer(y_train)[1:500]
+#  y_valid = as.integer(y_valid)[1:500]
+#  
+
+## -----------------------------------------------------------------------------
+#  example = array_reshape(x_train[1,], c(28,28))
+#  
+#  example %>% show_image(cmap = 'gray') %>% plot()
+
+## -----------------------------------------------------------------------------
+#  TensorDataset = torch()$utils$data$TensorDataset
+#  
+#  bs = 32
+#  train_ds = TensorDataset(tensor(x_train), tensor(y_train))
+#  valid_ds = TensorDataset(tensor(x_valid), tensor(y_valid))
+#  train_dl = TfmdDL(train_ds, bs = bs, shuffle = TRUE)
+#  valid_dl = TfmdDL(valid_ds, bs = 2 * bs)
+#  dls = Data_Loaders(train_dl, valid_dl)
+#  
+#  one = one_batch(dls)
+#  x = one[[1]]
+#  y = one[[2]]
+#  x$shape; y$shape
+#  
+#  nn = nn()
+#  Functional = torch()$nn$functional
+
+## -----------------------------------------------------------------------------
+#  model = nn_module(function(self) {
+#  
+#    self$lin1 = nn$Linear(784L, 50L, bias=TRUE)
+#    self$lin2 = nn$Linear(50L, 10L, bias=TRUE)
+#  
+#    forward = function(y) {
+#      x = self$lin1(y)
+#      x = Functional$relu(x)
+#      self$lin2(x)
+#    }
+#  })
+
+## -----------------------------------------------------------------------------
+#  learn = Learner(dls, model, loss_func=nn$CrossEntropyLoss(), metrics=accuracy)
+#  
+#  learn %>% summary()
+#  
+#  learn %>% fit_one_cycle(1, 1e-2)
 
