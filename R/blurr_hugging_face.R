850078bb1583a57437a87e5e54eba797f468937e
henry090 <turqut.a.314@gmail.com>
2020-11-13 17:28:58 +0400

    hugging face model and cbs wrappers

--- R/blurr_hugging_face.R
+++ R/blurr_hugging_face.R
@@ -104,6 +104,107 @@ get_hf_objects <- function(...) {
 
 
 
+#' @title HF_BaseModelWrapper
+#'
+#' @description Same as `nn.Module`, but no need for subclasses to call `super().__init__`
+#'
+#'
+#' @param hf_model model
+#' @param output_hidden_states output hidden states
+#' @param output_attentions output attentions
+#' @param ... additional arguments to pass
+#' @return None
+#' @export
+HF_BaseModelWrapper <- function(hf_model, output_hidden_states = FALSE,
+                                output_attentions = FALSE, ...) {
+
+  blurr()$modeling$all$HF_BaseModelWrapper(
+    hf_model = hf_model,
+    output_hidden_states = output_hidden_states,
+    output_attentions = output_attentions,
+    ...
+  )
+
+}
+
+
+#' HF_BaseModelCallback
+#'
+#'
+#' @param ... parameters to pass
+#' @return None
+#' @export
+HF_BaseModelCallback = function(...) {
+  args = list(...)
+
+  if(length(args)>0) {
+    do.call(blurr()$modeling$all$HF_BaseModelCallback, args)
+  } else {
+    blurr()$modeling$all$HF_BaseModelCallback
+  }
+}
+
+
+#' @title Hf_splitter
+#'
+#' @description Splits the huggingface model based on various model architecture conventions
+#'
+#'
+#' @param m parameters
+#' @return None
+#' @export
+hf_splitter <- function(m) {
+
+  if(missing(m)) {
+    blurr()$modeling$all$hf_splitter
+  } else {
+    blurr()$modeling$all$hf_splitter(
+      m = m
+    )
+  }
+
+}
+
+
+#' @title BLURR_MODEL_HELPER
+#'
+#'
+#'
+#'
+#'
+#' @return None
+#' @export
+helper = function() {
+  blurr()$data$all$BLURR_MODEL_HELPER
+}
+
+
+#' @title HF_TASKS_ALL
+#'
+#' @description An enumeration.
+#'
+#' @return None
+#' @export
+HF_TASKS_ALL <- function() {
+
+  blurr()$data$all$HF_TASKS_ALL
+
+}
+
+
+#' Auto configuration
+#'
+#'
+#' @return None
+#' @export
+AutoConfig = function() {
+  if(reticulate::py_module_available('transformers')) {
+    trf = reticulate::import('transformers')
+    trf$AutoConfig
+  }
+}
+
+
 
 
 
