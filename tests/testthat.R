2407dd8daae71bb11603ab9e36b1baa54e760161
Turgut <turqut.a.314@gmail.com>
2020-11-16 15:04:04 +0400

    predict test (#69)
    * predict medical
    
    * fix datalaoders. Defaults to 0 if Mac or Windows
    
    * gan requires cuda // skip
    
    * remove zealottt
    
    * add ggplot to tests
    
    * HF ops
    
    * update readme

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,12 @@
 
-options(reticulate.useImportHook = FALSE)
+
 library(testthat)
 library(fastai)
 library(reticulate)
 library(magrittr)
 library(data.table)
+library(ggplot2)
+library(ggpubr)
 
 test_check("fastai")
 
