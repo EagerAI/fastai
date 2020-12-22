8880a11a2e585731556a867fd22effb909ecbf31
henry090 <turqut.a.314@gmail.com>
2020-12-22 13:49:22 +0400

    fix

--- R/install_fastai.R
+++ R/install_fastai.R
@@ -42,21 +42,6 @@ install_fastai <- function(version, gpu = FALSE, cuda_version = '10.1', overwrit
   }
 
 
-  # only linux and mac, fix when https://github.com/fastaudio/fastaudio/issues/71
-  fastaudio_ = function()  {
-
-    if (!reticulate::py_module_available('fastaudio')) {
-      reticulate::py_install('fastaudio', pip = TRUE)
-
-      if(missing(version)) {
-        reticulate::py_install(paste("fastai","2.1.9",sep = "=="), pip = TRUE)
-      } else {
-        reticulate::py_install(paste("fastai",version,sep = "=="), pip = TRUE)
-      }
-    }
-
-  }
-
   if(length(extra_pkgs) > 0) {
     required_py_pkgs = c(required_py_pkgs, extra_pkgs, git_pkgs)
   }
@@ -96,8 +81,21 @@ install_fastai <- function(version, gpu = FALSE, cuda_version = '10.1', overwrit
 
   if(missing(version)) {
     required_py_pkgs = replace(required_py_pkgs, required_py_pkgs=="fastai", "fastai==2.1.9")
+    f_version = "fastai==2.1.9"
   } else {
     required_py_pkgs = replace(required_py_pkgs, required_py_pkgs=="fastai", paste("fastai",version,sep = "=="))
+    f_version = paste("fastai",version,sep = "==")
+  }
+
+  fastaudio_ = function()  {
+
+    if (!reticulate::py_module_available('fastaudio')) {
+      reticulate::py_install('fastaudio', pip = TRUE)
+
+      reticulate::py_install(f_version, pip = TRUE)
+
+    }
+
   }
 
   # kaggle import is different. We cannot check it with py module available.
