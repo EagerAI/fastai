3707717021f69255e488bca485e0f39c12b2ce27
Turgut Abdullayev <turqut.a.314@gmail.com>
2021-03-21 22:43:26 +0400

    fix tim learner

--- inst/python/fastaibuilt/pretrained_timm_models.py
+++ inst/python/fastaibuilt/pretrained_timm_models.py
@@ -4,6 +4,7 @@ from timm import create_model
 from fastai.vision.learner import _update_first_layer
 from fastai.vision.learner import _add_norm
 
+
 def create_body(arch, n_in=3, pretrained=True, cut=None):
     "Cut off the body of a typically pretrained `arch` as determined by `cut`"
     model = arch(pretrained=pretrained)
@@ -15,8 +16,6 @@ def create_body(arch, n_in=3, pretrained=True, cut=None):
     elif callable(cut): return cut(model)
     else:                           raise NamedError("cut must be either integer or a function")
 
-
-
 def create_timm_body(arch:str, pretrained=True, cut=None, n_in=3):
     "Creates a body from any model in the `timm` library."
     model = create_model(arch, pretrained=pretrained, num_classes=0, global_pool='')
@@ -27,8 +26,6 @@ def create_timm_body(arch:str, pretrained=True, cut=None, n_in=3):
     if isinstance(cut, int): return nn.Sequential(*list(model.children())[:cut])
     elif callable(cut): return cut(model)
     else: raise NamedError("cut must be either integer or function")
-
-
 
 
 def cnn_learner(dls, arch, loss_func=None, pretrained=True, cut=None, splitter=None,
@@ -50,13 +47,12 @@ def create_timm_model(arch:str, n_out, cut=None, pretrained=True, n_in=3, init=n
     "Create custom architecture using `arch`, `n_in` and `n_out` from the `timm` library"
     body = create_timm_body(arch, pretrained, None, n_in)
     if custom_head is None:
-        nf = num_features_model(nn.Sequential(*body.children())) * (2 if concat_pool else 1)
+        nf = num_features_model(nn.Sequential(*body.children()))
         head = create_head(nf, n_out, concat_pool=concat_pool, **kwargs)
     else: head = custom_head
     model = nn.Sequential(body, head)
     if init is not None: apply_init(model[1], init)
     return model
-
 
 def timm_learner(dls, arch:str, loss_func=None, pretrained=True, cut=None, splitter=None,
                 y_range=None, config=None, n_out=None, normalize=True, **kwargs):
@@ -71,6 +67,16 @@ def timm_learner(dls, arch:str, loss_func=None, pretrained=True, cut=None, split
     return learn
 
 
-#learn = timm_learner(dls, 'cspdarknet53',metrics=accuracy)
+
+
+
+
+
+
+
+
+
+
+
 
 
