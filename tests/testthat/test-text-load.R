81c198e52b583bf1a1685f912ce53be639e5bcc3
henry090 <turqut.a.314@gmail.com>
2021-05-14 21:08:09 +0400

    skip windows tests

--- tests/testthat/test-text-load.R
+++ tests/testthat/test-text-load.R
@@ -19,7 +19,7 @@ test_succeeds('read movie data', {
   df$sentiment<- as.factor(c(1, 1, 0, 0, 1, 1, 0, 0, 0, 1))
 })
 
-test_succeeds('text dataloader', {
+test_succeeds_windows('text dataloader', {
   data_block = DataBlock(
     blocks = list(TextBlock_from_df(text_cols="review"), CategoryBlock()),
     get_x = ColReader("text"),
@@ -30,7 +30,7 @@ test_succeeds('text dataloader', {
   data_iterator %>% one_batch()
 })
 
-test_succeeds('predict text with AWS', {
+test_succeeds_windows('predict text with AWS', {
   learnR <- language_model_learner(data_iterator, AWD_LSTM(), drop_mult = 0.3, metrics = list(accuracy, Perplexity()))
   learnR %>% predict(df$review[1])
 })
--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -17,6 +17,19 @@ test_succeeds <- function(desc, expr) {
   })
 }
 
+os_win_skip <- function() {
+  if (os()=='windows')
+    skip("skip windows")
+}
+
+test_succeeds_windows <- function(desc, expr) {
+  test_that(desc, {
+    skip_if_no_fastai_core()
+    skip_if_32bit()
+    os_win_skip()
+    expect_error(force(expr), NA)
+  })
+}
 
 
 
